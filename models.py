d93bcd013ba27f34ab447b054be353cd9f1a6751
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-23 13:39:15 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,30 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
 class Woreda(Base):
     __tablename__ = 'woredas'
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
+    name = Column(String, unique=True)
     kebeles = relationship("Kebele", back_populates="woreda", cascade="all, delete-orphan")
 
 class Kebele(Base):
     __tablename__ = 'kebeles'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String)
     woreda_id = Column(Integer, ForeignKey('woredas.id'))
     woreda = relationship("Woreda", back_populates="kebeles")
 
 class Farmer(Base):
     __tablename__ = 'farmers'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String)
+    f_type = Column(String)
     woreda = Column(String)
     kebele = Column(String)
     phone = Column(String)
-    audio_path = Column(String)
+    audio_url = Column(String)
     registered_by = Column(String)
 
 def create_tables():
