89472c5e1cbf114540f5dc758cab93def0e16a79
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-23 12:22:56 +0300

    Update comment for SQLite connection in database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# connect_args is needed for SQLite to work with Streamlit's multi-threading
+# connect_args is required for SQLite to work with Streamlit's multi-threading
 engine = create_engine('sqlite:///survey.db', connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
