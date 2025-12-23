0baa8c2c0f2325233b5d8f1f643d6ed411a98368
Derese4803 <derese.esubalew-c@oneacrefund.org>
2025-12-23 11:26:33 +0300

    Add autocommit option to SessionLocal

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# connect_args is needed for SQLite to work with Streamlit's multi-threading
 engine = create_engine('sqlite:///survey.db', connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
